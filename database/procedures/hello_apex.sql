2adbd59279954166438a36c30221770bf5aa8e5c
Abhinav Yadav <abhi08071999@gmail.com>
2026-09-20 19:58:57 +0530

    Update hello procedure message

--- database/procedures/hello_apex.sql
+++ database/procedures/hello_apex.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE PROCEDURE hello_apex
 IS
 BEGIN
-    DBMS_OUTPUT.PUT_LINE('Hello from Oracle APEX Git Project');
+    DBMS_OUTPUT.PUT_LINE('Hello Abhinav, welcome to Oracle APEX Git development');
 END hello_apex;
 /
